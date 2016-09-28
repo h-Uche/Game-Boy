9dfaa9731f5b7c7bc024fa89bccd6de805ae5271
h-Uche <winner.okeke.150302@unn.edu.ng>
2016-09-28 04:00:23 -0700

    Game Boy first commit

---
+++ GAME BOY/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAME_BOY
+{
+    class GameBoy
+    {
+       static void Main(string[] args)
+    {
+        /*my first program in C#*/
+        Console.WriteLine("Game Boy");
+        Console.ReadKey();
+    }
+}
+}
